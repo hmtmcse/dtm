9252b52a634eaa366b3656e2dc57f319325aa0f0
Touhid Mia <hmtm.cse@gmail.com>
2019-02-11 22:28:28 +0600

    Session timeout Login Implementation

--- react-app/src/artifacts/ra-static-holder.js
+++ react-app/src/artifacts/ra-static-holder.js
@@ -1,6 +1,7 @@
 
 export default class RaStaticHolder {
     static message = {};
+    static requestStack = {};
 
     static addMessageData(message, isSuccess = true){
         this.message = {isSuccess: isSuccess, message: message}
--- react-app/src/views/authentication/login-dialog.js
+++ react-app/src/views/authentication/login-dialog.js
@@ -5,15 +5,33 @@ import LoginFormProcessor from "./login-form-processor";
 import React, {Component} from 'react';
 import {withStyles} from "@material-ui/core";
 import {LoginLayoutJss} from "../../assets/jss/login-layout-jss";
+import RaStaticHolder from "../../artifacts/ra-static-holder";
 
 class LoginDialog extends Component {
 
+    callback = response => {
+        const {parent} = this.props;
+        let data = response.data;
+        if (data.isSuccess) {
+            parent.setState({showLoginPopup: false});
+            Object.entries(RaStaticHolder.requestStack).map(([objectKey, objectValue], key) => {
+                parent.callToApiByAxios(objectValue.dataSet, objectValue.success, objectValue.failed)
+            });
+            RaStaticHolder.requestStack = {};
+        } else {
+            parent.showErrorInfo("Sorry Unable to Login.");
+        }
+    };
+
     render() {
         const {parent, classes} = this.props;
+        if (parent.state.loginSuccessData.dataSet.url) {
+            RaStaticHolder.requestStack[parent.state.loginSuccessData.dataSet.url] = parent.state.loginSuccessData;
+        }
         return (
             <Dialog open={true} fullWidth>
                 <DialogContent className={classes.marginBottomForPopup} >
-                    <LoginFormProcessor parent={parent}/>
+                    <LoginFormProcessor parent={parent} callBack={this.callback}/>
                 </DialogContent>
             </Dialog>
         );
